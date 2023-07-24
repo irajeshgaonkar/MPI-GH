32e93b0f2cc4e57a98e80982f39c493874812f7d
mallesh byri <mallesh.byri@hca.wa.gov>
2023-07-24 17:37:34 +0000

    -- updated custom_data-mapping table script

---
+++ HCA.Data/release_phase_3.1.1.sql
@@ -0,0 +1,12 @@
+﻿	-- update custom_data_mapping table values with new requested naming convention 
+	 
+	 DO $$
+  BEGIN
+	  IF (select id from coalitionmpi.custom_data_mapping where id = 1) = 1
+	  THEN	 
+		update coalitionmpi.custom_data_mapping set verato_request_path = 'custom.CreateDates$createdDate'
+		, verato_response_path = 'custom.CreateDates$createdDate'
+		, api_response_path = 'custom.CreateDates$createdDate'
+		Where id = 1;
+	  END IF;
+  END $$;
