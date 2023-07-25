3a75928ee4a3e6c9cd41452e050f9e54cfb611aa
mallesh byri <mallesh.byri@hca.wa.gov>
2023-07-25 16:51:19 +0000

    -- update custom_data_mapping script

--- HCA.Data/release_phase_3.1.1.sql
+++ HCA.Data/release_phase_3.1.1.sql
@@ -4,9 +4,11 @@
   BEGIN
 	  IF (select id from coalitionmpi.custom_data_mapping where id = 1) = 1
 	  THEN	 
-		update coalitionmpi.custom_data_mapping set verato_request_path = 'custom.CreateDates$createdDate'
+		update coalitionmpi.custom_data_mapping set input_column_name = 'createDate'
+		, verato_request_path = 'custom.CreateDates$createdDate'
 		, verato_response_path = 'custom.CreateDates$createdDate'
 		, api_response_path = 'custom.CreateDates$createdDate'
+		, output_column_name = 'createDate'
 		Where id = 1;
 	  END IF;
   END $$;
