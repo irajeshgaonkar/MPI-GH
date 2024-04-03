ef894bfdb52b6fd703ba7c79da0a7da06d609506
Kiran Kuchana <kiran.kuchana@hca.wa.gov>
2024-04-03 18:41:58 +0000

    error message fix

--- HCA.Api/src/HCA.Api/Filters/IPValidationFilter.cs
+++ HCA.Api/src/HCA.Api/Filters/IPValidationFilter.cs
@@ -34,19 +34,19 @@ namespace HCA.Api.Filters
 
                 if (string.IsNullOrEmpty(sourceSystem))
                 {
-                    context.Result = new BadRequestObjectResult(new { message="sourceSystem Validation  is failed , There is no sourcesystem filed is present in the input"});
+                    context.Result = new OkObjectResult(new { errorCode = "400" ,message ="sourceSystem Validation  is failed , There is no sourcesystem field is present in the input"});
                     return;
                 }
                 if (string.IsNullOrEmpty(ipAddress))
                 {
-                    context.Result = new BadRequestObjectResult(new { message = "ipAddress Validation is failed , There is no ipAddress value present in input"});
+                    context.Result = new OkObjectResult(new { errorCode = "400", message = "ipAddress Validation is failed , There is no ipAddress value present in the input"});
                     return;
                 }
 
                 bool isTrusted = await _iPConfigRepository.IsIPAddressTrustedAsync(sourceSystem, ipAddress);
                 if (!isTrusted)
                 {
-                    context.Result = new BadRequestObjectResult(new {Message = "IPAddress/SourceSytem name Validation failed. Received IPAddress/SourceSystem names from input are not matched." });
+                    context.Result = new OkObjectResult(new { errorCode = "400", Message = "IPAddress/SourceSytem name Validation failed. Received IPAddress/SourceSystem names from input are not matched." });
                     return;
                 }
                 await next();
