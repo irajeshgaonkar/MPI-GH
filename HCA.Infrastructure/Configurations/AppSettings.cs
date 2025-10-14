086bc31828bf91397c3012f19ea62dc5db5afd4d
Rajesh <Gaonkar.Rajesh@hca.wa.gov>
2025-10-14 16:45:26 -0700

    Adding Verato options

--- HCA.Infrastructure/Configurations/AppSettings.cs
+++ HCA.Infrastructure/Configurations/AppSettings.cs
@@ -3,12 +3,21 @@
     public class AppSettings
     {
         public string DbConnectionStr { get; set; }
+
         public LoggingOptions Logging { get; set; }
+
         public MuleSoftOptions MuleSoft { get; set; }
+
         public string InputBucketName { get; set; }
+
         public string OutputBucketName { get; set; }
+
         public SqsOptions SqsOptions { get; set; }
+
         public SftpOptions SftpOptions { get; set; }
+
         public SecurityOptions SecurityOptions { get; set; }
+
+        public VeratoOptions VeratoOptions { get; set; }
     }
 }
---
+++ HCA.Infrastructure/Configurations/VeratoOptions.cs
@@ -0,0 +1,21 @@
+﻿namespace HCA.Infrastructure.Configurations
+{
+    public class VeratoOptions
+    {
+        public string BaseUrl { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string EnrichBaseUrl { get; set; }
+
+        public string EnrichUsername { get; set; }
+
+        public string EnrichPassword { get; set; }
+
+        public string ClientCert { get; set; }
+
+        public string ClientCertPassword { get; set; }
+    }
+}
