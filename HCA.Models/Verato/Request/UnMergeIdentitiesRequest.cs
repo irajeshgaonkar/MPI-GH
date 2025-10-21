b5b190c8b4596815931e542ac35bdd14df51f96c
V Furman <v.furman@hca.wa.gov>
2025-10-21 14:30:57 -0700

    simplify constructor

--- HCA.Models/Verato/Request/UnMergeIdentitiesRequest.cs
+++ HCA.Models/Verato/Request/UnMergeIdentitiesRequest.cs
@@ -5,19 +5,15 @@ namespace HCA.Models.Verato.Request;
 /// <summary>
 /// Un merge identities request
 /// </summary>
-public class UnMergeIdentitiesRequest : VeratoRequest
+/// <remarks>
+/// <see cref="UnMergeIdentitiesRequest"/>
+/// </remarks>
+/// <param name="trackingId">Tracking id</param>
+public class UnMergeIdentitiesRequest( string trackingId, UnMergingSources content ) : VeratoRequest(trackingId)
 {
-    /// <summary>
-    /// <see cref="UnMergeIdentitiesRequest"/>
-    /// </summary>
-    /// <param name="trackingId">Tracking id</param>
-    public UnMergeIdentitiesRequest(string trackingId, UnMergingSources content) : base(trackingId)
-    {
-        Content = content;
-    }
 
     /// <summary>
     /// Un merging sources
     /// </summary>
-    public UnMergingSources Content { get; set; }
+    public UnMergingSources Content { get; set; } = content;
 }
